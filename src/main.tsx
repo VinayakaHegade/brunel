a70b4ab87f1cfa7952c12b5cdd2042526fe271b0
Vinayak <myselfvinayakhegde@gmail.com>
2024-05-09 11:53:38 +0530

    feat: disable app for small screens

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
@@ -16,8 +16,25 @@ const router = createBrowserRouter([
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+const RootComponent = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowWidth > 850 ? (
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  ) : (
+    <div className="desktop-warning">
+      This website is currently not available for small screens. Please open or
+      use it on a desktop.
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<RootComponent />);
